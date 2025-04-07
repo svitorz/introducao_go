38c78ac7dda2c48eacb52442fe7263f00eca1c1b
svitorz <vitor.fabio.castro@gmail.com>
2025-04-07 18:30:13 -0300

    feat: trabalhando com arrays e slices

---
+++ Arrays e Slices/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	array1 := [5]string{"v", "i", "t", "o", "r"}
+	fmt.Println(array1)
+
+	// slices
+	slice := []int{10, 11, 12, 13, 14, 15, 16, 17, 18}
+	fmt.Println(slice)
+}
