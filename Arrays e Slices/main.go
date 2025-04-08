5ff6843b5cd5e6545b6c2b2a714d31da0b5908b3
svitorz <vitor.fabio.castro@gmail.com>
2025-04-07 22:02:04 -0300

    feat: Trabalhando com Arrays Internos

--- Arrays e Slices/main.go
+++ Arrays e Slices/main.go
@@ -9,4 +9,15 @@ func main() {
 	// slices
 	slice := []int{10, 11, 12, 13, 14, 15, 16, 17, 18}
 	fmt.Println(slice)
+
+	// Arrays internos
+	slice2 := make([]float32, 10, 15)
+	fmt.Println(slice2)
+	fmt.Println(len(slice2)) // tamanho
+	fmt.Println(cap(slice2)) // capacidade
+	/**
+	* Se o tamanho do array interno "estourar", o compilador irá duplicá-lo, consequentemente, dobrando de tamanho.
+	* */
+	//podemos resumir que o array tem um tamanho fixo, e o slice não precisa ter.
+	//Já o array, pode "dobrar" de tamanho de acordo com a necessidade.
 }
