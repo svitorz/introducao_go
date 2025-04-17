b5ab225c640d713b82cc55e0cf998ce16807ce8a
svitorz <vitor.fabio.castro@gmail.com>
2025-04-17 00:58:46 -0300

    feat: Utilizando padrão select

---
+++ 12.Concurrence/Select/select.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	canal1, canal2 := make(chan string), make(chan string)
+
+	go func() {
+		for {
+			time.Sleep(time.Millisecond * 500)
+			canal1 <- "Canal 1"
+		}
+	}()
+
+	go func() {
+		for {
+			time.Sleep(time.Second * 2)
+			canal2 <- "Canal 2"
+		}
+	}()
+
+	for {
+		/**
+		* Neste caso, os canais sao executados assim que receberem as devidas mensagens, evitando perdas de tempo.
+		* Os canais executam suas funções assim que recebem valor.
+		* */
+		select {
+		case mensagemCanal1 := <-canal1:
+			fmt.Println(mensagemCanal1)
+		case mensagemCanal2 := <-canal2:
+			fmt.Println(mensagemCanal2)
+		}
+	}
+}
