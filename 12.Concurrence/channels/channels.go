2f7fa26e1b955aabd3b10cbc823339829069ce59
svitorz <vitor.fabio.castro@gmail.com>
2025-04-17 00:20:08 -0300

    feat: Nova sintaxe nos canais

--- 12.Concurrence/channels/channels.go
+++ 12.Concurrence/channels/channels.go
@@ -10,19 +10,28 @@ func main() {
 	go escrever("Olá, Mundo!", canal)
 
 	fmt.Println("Depois da função escrever começar a ser executada")
-	for {
-		mensagem, aberto := <-canal
 
-		if !aberto {
-			break
-		}
+	/**
+	* for {
+	*	mensagem, aberto := <-canal
 
+	*	if !aberto {
+	*		break
+	*	}
+
+	*	fmt.Println(mensagem)
+	*}
+	 */
+
+	// mesma função, mas com sintaxe diferente
+	for mensagem := range canal {
 		fmt.Println(mensagem)
 	}
 }
 
 func escrever(text string, canal chan string) {
 	// time.Sleep(time.Second * 5)
+
 	for i := 0; i <= 5; i++ {
 		canal <- text
 		time.Sleep(time.Second)
