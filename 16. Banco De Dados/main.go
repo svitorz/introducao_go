0d4b344932209883ce9bdbb86ef2a364e34500a6
svitorz <vitor.fabio.castro@gmail.com>
2025-04-18 16:25:47 -0300

    feat: trabalhando com a banco de dados

---
+++ 16. Banco De Dados/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	urlConexao := "root:mydba@/introducao_go?charset=utf8&parseTime=true&loc=Local"
+	db, err := sql.Open("mysql", urlConexao)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Conexão aberta.")
+}
